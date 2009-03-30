49b4d539c0398fb0fffdc2c7687444a534b3d557
Ross Dakin <rossdakin@gmail.com>
2009-03-30 03:42:46 -0700

    Progress bar updates: docstring and some semaphores.

--- django_cloudfiles/utils/progress_bar.py
+++ django_cloudfiles/utils/progress_bar.py
@@ -10,6 +10,20 @@ BACKSPACE = '\b'
 class ProgressBar(object):
     """
     Dirt simple command-line progress bar.
+
+    Exports three public methods:
+     * start(self) - call this to start the progress bar
+     * end(self) - call this to end the progress bar (fills it to 100%)
+     * tick(self, progress, total) - call this to make the progress bar grow to
+                                     progress/total of its full length
+
+    Allows for some customization by specifying in the constructor the
+    characters used in the progress bar proper and the total bar length:
+     * total_ticks
+     * begin_char
+     * end_char
+     * run_char
+     * tip_char
     """
     def __init__(self, total_ticks=TOTAL_TICKS, begin_char=BEGIN_CHAR,
                  end_char=END_CHAR, run_char=RUN_CHAR, tip_char=TIP_CHAR):
@@ -19,17 +33,21 @@ class ProgressBar(object):
         self.run_char = run_char
         self.tip_char = tip_char
         self._ticks_printed = 0
-        self.done = False
+        self._started = False
+        self._ended = False
 
     def _print_tick(self):
         write("%s%s%s" % (BACKSPACE, self.run_char, self.tip_char))
         self._ticks_printed += 1
 
     def start(self):
-        write("  %s%s" % (self.begin_char, self.end_char))
+        if self._started:
+            return
+        self._started = True
+        write("  %s%s" % (self.begin_char, self.tip_char))
 
     def tick(self, progress, total):
-        if self.done:
+        if self._ended or not self._started:
             return
         if progress == total:
             self.end()
@@ -39,8 +57,8 @@ class ProgressBar(object):
             self._print_tick()
 
     def end(self):
-        if not self.done:
-            self.done = True
+        if self._started and not self._ended:
+            self._ended = True
             while self._ticks_printed < self.total_ticks:
                 self._print_tick()
             print self.end_char
